6199d1642df7a874ac0454487ec9f773dee78772
Your Name <https://github.qmul.ac.uk/ec23166>
2025-03-01 21:36:13 +0000

    Moved gitignore file outside from .idea folder

--- WeatherApp/src/components/Header.jsx
+++ WeatherApp/src/components/Header.jsx
@@ -3,14 +3,18 @@ import "./styles/Header.css";
 import WeatherIcon from "./WeatherIcon";
 
 const Header = () => {
+
+  const inputRef = React.useRef();
+
   return (
     <div className="header">
       <div className="darkModeToggle">
         <WeatherIcon condition="moonb" size="clamp(1rem, 50%, 2rem)" />
       </div>
       <div className="searchBar">
-        <WeatherIcon condition="search" size="clamp(1rem, 50%, 2rem)" />
+        <WeatherIcon condition="search" size="clamp(1rem, 50%, 2rem)" onClick={() => onSearch(inputRef.current.value)} />
         <input
+          ref={inputRef}
           type="text"
           className="searchField"
           placeholder="Enter Location, Postcode"
--- WeatherApp/src/components/Main.jsx
+++ WeatherApp/src/components/Main.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/Main.css";
+import Header from "./Header";
 import WeatherIcon from "./WeatherIcon";
+import cloudyIcon from "../assets/cloudy.png";
 
 const Main = () => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const search = async (city) => {
+    if (city.trim() === "") {
+      alert("Please enter a city name");
+      return;
+    }
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_KEY}`;
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (!response.ok) {
+        alert(data.message);
+        return;
+      }
+
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        high: Math.floor(data.main.temp_max),
+        low: Math.floor(data.main.temp_min),
+        icon: cloudyIcon,
+      });
+    } catch (error) {
+      setWeatherData(null);
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+  useEffect(() => {
+    search("New York");
+  }, []);
+
   return (
     <div className="main_container">
       <div className="main">
@@ -10,17 +48,23 @@ const Main = () => {
           <p>Fri, 28 February, 2025</p>
           <p>1:34 PM</p>
         </div>
-        <div className="weather">
-          <WeatherIcon condition="cloudy" size="clamp(4rem, 50%, 15rem)" />
-          <div>
-            <h1>16°C</h1>
-            <p>Tower Hamlets</p>
-          </div>
-        </div>
-        <div className="high_low">
-          <p>H: 18°C</p>
-          <p>L: 10°C</p>
-        </div>
+        {weatherData ? (
+          <>
+            <div className="weather">
+              <WeatherIcon condition="cloudy" size={225} />
+              <div>
+                <h1>{weatherData.temperature}°C</h1>
+                <p>{weatherData.location}</p>
+              </div>
+            </div>
+            <div className="high_low">
+              <p>H: {weatherData.high}°C</p>
+              <p>L: {weatherData.low}°C</p>
+            </div>
+          </>
+        ) : (
+          <p>Loading weather data...</p>
+        )}
       </div>
     </div>
   );
